72393acde735cd609cf0d3c753c99bc8fca2159c
ericjiji3 <eric.ji3333@gmail.com>
2025-02-26 21:02:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+// IMPORT PAGES HERE
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <h1>COMPONENTS</h1>
+      <div>
+        {/* IMPORT COMPONENTS HERE */}
+      </div>
+      
+      <h1>PAGES</h1>
+      <div>
+        {/* IMPORT PAGES HERE */}
+      </div>
+     </div>
+     
+    </>
+  )
+}
+
+export default App
