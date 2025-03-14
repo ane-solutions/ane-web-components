0998da8e23e82e178d205bb5721614c251bf64a8
Arjun Patel <patelarjun66@gmail.com>
2025-03-14 18:20:05 -0500

    update app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
 import React from 'react';
-import Home1 from './pages/Home/Home1/Home1';
 import Home2 from './pages/Home/Home2/Home2';
-import Home3 from './pages/Home/Home3/Home3';
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Home1 /> */}
-      {/* <Home2 /> */}
-      <Home3 />
+      <Home2 />
     </div>
   )
 }
